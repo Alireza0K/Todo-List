b5070081baefc248883f04e0a9524da7bbebf388
Alireza Karimi <Alireza.Karimi.Programmer@gmail.com>
2022-04-04 01:58:52 +0430

    Some Edit in UI and Complete

--- template/main_page.php
+++ template/main_page.php
@@ -18,67 +18,80 @@
         <!-- This section about content in body -->
         <div class="body">
             <div class="main">
-                <div class="signout_btn">
-                    <button class="btn signout_btn">signout</button>
-                </div>
-                <div class="grid_boxes">
-                    <!-- box1 content -->
-                    <div class="box1">
-                        <div class="box1-content">
-                            <img class="avatar" src="assets/image/IMG_20211004_075621.jpg" alt="" width="90px" height="90px">
-                            <p class="name">Example name</p>
-                            <p class="email">Example@gmail.com</p>
-                        </div>
+                <div class="section-1">
+                    <div class="signout_btn">
+                        <button class="btn signout_btn">signout</button>
                     </div>
-                    <!-- box2 content -->
-                    <div class="box2">
-                        <div class="box2-content">
-                            <div class="head-box2">
-                                <h2 class="content-box2">All Tasks</h2>
-                                <button class="btn content-box2 btn-add">Add Task</button>
-                            </div>
-                            <!-- Box2 content about Tasks -->
-                            <div class="tasks-box-storage">
-                                <div class="task">
-                                    <div class="left-content">
-                                        <h3 class="title-task" >Task Title</h3>
-                                        <div><hr class="hrwidth"></div>
-                                        <br>
-                                        <p class="discription">Discription</p>
-                                    </div>
-                                    <div class="right-content">
-                                        <button class="btn btn-delete btn-in"><span><img src="assets/image/trash-2.svg" alt="" width="25px"></span></button>
-                                        <button class="btn btn-success btn-in"><span><img src="assets/image/check.svg" alt="" width="25px"></span></button>
-                                    </div>
-                                </div>
+                    <div class="grid_boxes">
+                        <!-- box1 content -->
+                        <div class="box1">
+                            <div class="box1-content">
+                                <img class="avatar" src="assets/image/IMG_20211004_075621.jpg" alt="" width="90px" height="90px">
+                                <p class="name">Example name</p>
+                                <p class="email">Example@gmail.com</p>
                             </div>
                         </div>
-                    </div>
-                    <!-- box3 content -->
-                    <div class="box3">
-                        <div class="box3-content">
-                            <div class="title-mods">
-                                <h2>Mods</h2>
-                            </div>
-                            <!-- Mods Boxes -->
-                            <div class="mod-boxes">
-                                <div class="mod" style="background-color: #8FBDD3 ;">
-                                    <div class="mod-icon">
-                                        <img src="assets/image/home.svg" alt="" width="50px" style="color: white;">
-                                    </div>
-                                    <div class="texts" style="color: b;">
-                                        <h3>Mod name</h3>
-                                        <p>Discription</p>
-                                    </div>
-                                </div>
-                            </div>
+                        <!-- box2 content -->
                         
+                        <!-- box3 content -->
+                        <div class="box3">
+                            <div class="box3-content">
+                                <div class="title-mods">
+                                    <h2>Mods</h2>
+                                </div>
+                                <!-- Mods Boxes -->
+                                <div class="mod-boxes">
+                                    <div class="mod" style="background-color: #8FBDD3 ;">
+                                        <div class="mod-icon">
+                                            <img src="assets/image/home.svg" alt="" width="50px" style="color: white;">
+                                        </div>
+                                        <div class="texts" style="color: b;">
+                                            <h3>Mod name</h3>
+                                            <p>Discription</p>
+                                        </div>
+                                    </div>
+                                    
+                                </div>
+                            
+                            </div>
                         </div>
+                        
                     </div>
-                    
                 </div>
+            </div>
+        
+            <div class="section-1 part2">
+                <div class="box2">
+                                <div class="box2-content">
+                                    <div class="head-box2">
+                                        <h2 class="content-box2">All Tasks</h2>
+                                        <button class="btn content-box2 btn-add">Add Task</button>
+                                    </div>
+                                    <!-- Box2 content about Tasks -->
+                                    <div class="tasks-box-storage">
+                                        <!-- Tasks -->
+                                        <div class="task">
+                                            <div class="left-content">
+                                                <h3 class="title-task" >Task Title</h3>
+                                                <div><hr class="hrwidth"></div>
+                                                <br>
+                                                <p class="discription">Discription</p>
+                                            </div>
+                                            <div class="right-content">
+                                                <button class="btn btn-delete btn-in"><span><img src="assets/image/trash-2.svg" alt="" width="25px"></span></button>
+                                                <button class="btn btn-success btn-in"><span><img src="assets/image/check.svg" alt="" width="25px"></span></button>
+                                            </div>
+                                        </div>
+                                        
+                                    </div>
+                                </div>
+                            </div> 
+                    </div>
             </div>
         </div>
         <!-- This section about content in body -->
     </body>
 </html>
+
+
+
