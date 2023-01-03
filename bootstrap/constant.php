6776d703bfdfbd4c2155e8e85755090c504e4491
Alireza0K <alireza.karimi.programmer@gmail.com>
2023-01-03 18:54:40 +0330

    Start Rebuild

--- bootstrap/constant.php
+++
@@ -1,7 +0,0 @@
-<?php 
-
-# You can put your base url here
-define("BASE_URL" , "http://code-pure-php/Todo-List/");
-
-# You can put your base path here
-define("BASE_PATH" , "C:/xampp/htdocs/Code-Pure-PHP/Todo-List");
