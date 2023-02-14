57d1d3d5d11781f1290545d659ff66806c291a73
Alireza0K <alireza.karimi.programmer@gmail.com>
2023-02-14 19:50:37 +0330

    add rules to update process

--- app/Http/Controllers/UpdateTaskController.php
+++ app/Http/Controllers/UpdateTaskController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UpdateTaskRequest;
 use App\Models\Task;
 use Illuminate\Http\Request;
 
@@ -10,10 +11,11 @@ class UpdateTaskController extends Controller
     public function editTask(Request $request,Task $taskId)
     {
         $taskInfo = $taskId;
+
         return view("task.updateTask", compact("taskInfo"));
     }
 
-    public function update(Request $request,Task $taskId)
+    public function update(UpdateTaskRequest $request,Task $taskId)
     {
         $taskId->update($request->all());
 
